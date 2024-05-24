1779ee1de841b52402815323490c86ec95824b75
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-05-24 14:50:37 +0800

    Updates

--- assets/debounce-4335a2cb.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-04d98d10.js";const c=({value:e})=>{const[r,u]=t.useState(e);return t.useEffect(()=>{const o=setTimeout(()=>{u(e)},1e3);return()=>clearTimeout(o)},[e]),r};export{c as u};

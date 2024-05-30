16c5f6207f1d19f1c18eab5344ad596029be3222
iannico322 <iannicocaulin@gmail.com>
2024-05-30 09:46:09 +0800

    Updates

---
+++ assets/debounce-89fd5a53.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-07a75f65.js";const c=({value:e})=>{const[r,u]=t.useState(e);return t.useEffect(()=>{const o=setTimeout(()=>{u(e)},1e3);return()=>clearTimeout(o)},[e]),r};export{c as u};
